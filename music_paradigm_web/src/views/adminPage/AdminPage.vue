54bee88c79df6802ee5a5d5bef3b1de229dde40a
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-08-04 11:57:11 -0400

    Add the highlighting of the current tab in the sibar for the normal users and admin

--- music_paradigm_web/src/views/adminPage/AdminPage.vue
+++ music_paradigm_web/src/views/adminPage/AdminPage.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="page-grid">
     <div id="sidebar" class="sidebar-position sidebar">
-      <router-link :to="{name: 'admin.home'}">Home</router-link>
-      <router-link :to="{name: 'admin.users'}">Users</router-link>
-      <router-link :to="{name: 'admin.experiments'}">Experiments</router-link>
-      <router-link :to="{name: 'admin.curriculums'}">Curriculums</router-link>
-      <router-link :to="{name: 'admin.ressources'}">Experiment Ressources</router-link>
-      <router-link :to="{name: 'admin.data'}">Data</router-link>
-      <router-link :to="{name: 'admin.account'}">Account</router-link>
+      <router-link
+        :to="{name: 'admin.home'}"
+        :class="{'currentLink' : (activePage === 'home')}"
+      >Home</router-link>
+      <router-link
+        :to="{name: 'admin.users'}"
+        :class="{'currentLink' : (activePage === 'users')}"
+      >Users</router-link>
+      <router-link
+        :to="{name: 'admin.experiments'}"
+        :class="{'currentLink' : (activePage === 'experiments')}"
+      >Experiments</router-link>
+      <router-link
+        :to="{name: 'admin.curriculums'}"
+        :class="{'currentLink' : (activePage === 'curriculums')}"
+      >Curriculums</router-link>
+      <router-link
+        :to="{name: 'admin.ressources'}"
+        :class="{'currentLink' : (activePage === 'ressources')}"
+      >Ressources</router-link>
+      <router-link
+        :to="{name: 'admin.data'}"
+        :class="{'currentLink' : (activePage === 'data')}"
+      >Data</router-link>
+      <router-link
+        :to="{name: 'admin.account'}"
+        :class="{'currentLink' : (activePage === 'account')}"
+      >Account</router-link>
     </div>
     <div id="user-main" class="main-position">
-      <router-view class="main-display"/>
+      <router-view class="main-display" />
     </div>
   </div>
 </template>
@@ -19,7 +40,40 @@
 import "@/styles/loggedInPageTemplate.css";
 
 export default {
-  name: "UserPage",
+  name: "AdminPage",
+  data() {
+    return {
+      activePage: "undefined",
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  watch: {
+    // On change of the route, we reevaluate the current page
+    $route: {
+      immediate: true,
+      handler(to) {
+        let currentPage = "undefined";
+        const pageList = [
+          "home",
+          "users",
+          "experiments",
+          "curriculums",
+          "ressources",
+          "data",
+          "account",
+        ];
+        for (var i in pageList) {
+          var page = pageList[i];
+          if (to.name === `admin.${page}`) {
+            currentPage = page;
+          }
+        }
+        this.activePage = currentPage;
+      },
+    },
+  },
 };
 </script>
 
--- music_paradigm_web/src/views/userPage/UserPage.vue
+++ music_paradigm_web/src/views/userPage/UserPage.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="page-grid">
     <div id="sidebar" class="sidebar-position sidebar">
-      <router-link :to="{name: 'user.home'}">Home</router-link>
-      <router-link :to="{name: 'user.experiments'}">Experiments</router-link>
-      <router-link :to="{name: 'user.account'}">Account</router-link>
+      <router-link
+        :to="{ name: 'user.home' }"
+        :class="{'currentLink' : (activePage === 'home')}"
+      >Home</router-link>
+      <router-link
+        :to="{ name: 'user.experiments' }"
+        :class="{'currentLink' : (activePage === 'experiments')}"
+      >Experiments</router-link>
+      <router-link
+        :to="{ name: 'user.account' }"
+        :class="{'currentLink' : (activePage === 'account')}"
+      >Account</router-link>
     </div>
     <div id="user-main" class="main-position">
       <router-view class="main-display" />
@@ -16,6 +25,31 @@ import "@/styles/loggedInPageTemplate.css";
 
 export default {
   name: "UserPage",
+  data() {
+    return {
+      activePage: "undefined",
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  watch: {
+    // On change of the route, we reevaluate the current page
+    $route: {
+      immediate: true,
+      handler(to) {
+        let currentPage = "undefined";
+        const pageList = ["home", "experiments", "account"];
+        for (var i in pageList) {
+          var page = pageList[i];
+          if (to.name === `user.${page}`) {
+            currentPage = page;
+          }
+        }
+        this.activePage = currentPage;
+      },
+    },
+  },
 };
 </script>
 
