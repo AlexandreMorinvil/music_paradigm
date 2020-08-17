575b610a7e510afda8c893387881d33e1279d7a5
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-08-16 22:48:46 -0400

    Prepare backend folder to contain the logic for the curriculums

---
+++ music_paradigm_api/curriculum/curriculuims.controller.js
@@ -0,0 +1,70 @@
+﻿const express = require('express');
+const router = express.Router();
+const jwtAuthorize = require('jwt/jwt.authorization');
+const role = require('_helpers/role');
+const service = require('./curriculums.service');
+
+// routes
+router.get('/',                 jwtAuthorize(role.admin), getListAllHeaders);
+router.get('/:id',              jwtAuthorize(role.admin), getById);
+router.get('/description/:id',  jwtAuthorize(role.admin), getDescription);
+
+router.post('/',                jwtAuthorize(role.admin), create);
+
+router.put('/:id',              jwtAuthorize(role.admin), update);
+
+module.exports = router;
+
+function getListAllHeaders(req, res, next) {
+    service.getListAllHeaders()
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({ message: error.message }))
+        .finally(() => next());
+}
+
+function getById(req, res, next) {
+    service.getById(req.params.id)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({ message: error.message }))
+        .finally(() => next());
+}
+
+function getDescription(req, res, next) {
+    service.getDescriptionFromId(req.params.id)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({ message: error.message }))
+        .finally(() => next());
+}
+
+function create(req, res, next) {
+    service.create(req.body)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({ message: error.message }))
+        .finally(() => next());
+}
+
+function update(req, res, next) {
+    service.create(req.params.id, req.body)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({ message: error.message }))
+        .finally(() => next());
+}
+
+
+// // routes
+
+// router.delete('/:id',   jwtAuthorize(role.admin),   _delete);
+
+// module.exports = router;
+
+// function getCurrent(req, res, next) {
+//     service.getById(req.user.sub)
+//         .then(user => user ? res.json(user) : res.sendStatus(404))
+//         .catch(err => next(err));
+// }
+
+// function _delete(req, res, next) {
+//     service.delete(req.params.id)
+//         .then(() => res.json({}))
+//         .catch(err => next(err));
+// }
