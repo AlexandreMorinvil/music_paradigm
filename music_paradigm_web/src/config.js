6edcd6f469be0bc953b64cd7ed9d03460b44fb1d
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-10-20 20:55:16 -0400

    Modified the API URL base for it to correspond to hte API of the twins's experiment

--- music_paradigm_web/src/config.js
+++ music_paradigm_web/src/config.js
@@ -1,7 +1,4 @@
 ﻿const apiUrl = process.env.NODE_ENV === 'production' ? `https://paradigm.neuro.ki.se` : `${location.protocol}//${location.hostname}:4000`;
-console.log(process.env);
-console.log(process.env.NODE_ENV);
-console.log(apiUrl)
 
 export default {
     apiUrl: apiUrl,
