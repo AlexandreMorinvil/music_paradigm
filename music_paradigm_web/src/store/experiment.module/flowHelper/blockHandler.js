cc9cdebfb32290c3e778a70e5858efe5ba0cbd89
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-12-29 16:24:15 -0500

    Refactored the variable population function

--- music_paradigm_web/src/store/experiment.module/flowHelper/blockHandler.js
+++ music_paradigm_web/src/store/experiment.module/flowHelper/blockHandler.js
@@ -27,24 +27,11 @@ function getCurrentBlock(flow, cursor) {
 
 function populateVariables(block) {
 	const variables = experimentStoreState.variables;
-	const blocToUse = JSON.parse(JSON.stringify(block));
-	for (const section in blocToUse) {
-		if (typeof blocToUse[section] === 'string') {
-			for (const variable in variables.variables) blocToUse[section] = blocToUse[section].replace(variable, variables.value[variable]);
-		} else if (Array.isArray(blocToUse[section])) {
-			for (const index in blocToUse[section]) {
-				if (typeof blocToUse[section][index] === 'string') {
-					for (const variable in variables.value) blocToUse[section][index] = blocToUse[section][index].replace(variable, variables.value[variable]);
-				} else if (Array.isArray(blocToUse[section][index])) {
-					for (const subIndex in blocToUse[section][index]) {
-						if (typeof blocToUse[section][index][subIndex] === 'string') {
-							for (const variable in variables.value)
-								blocToUse[section][index][subIndex] = blocToUse[section][index][subIndex].replace(variable, variables.value[variable]);
-						}
-					}
-				}
-			}
-		}
+	const blockToUse = JSON.parse(JSON.stringify(block));
+	for (const section in blockToUse) {
+		if (typeof blockToUse[section] === 'string')
+			for (const variable in variables.value) blockToUse[section] = blockToUse[section].replace(variable, variables.value[variable]);
+		else if (Array.isArray(blockToUse[section])) blockToUse[section] = populateVariables(block[section]);
 	}
-	return blocToUse;
+	return blockToUse;
 }
