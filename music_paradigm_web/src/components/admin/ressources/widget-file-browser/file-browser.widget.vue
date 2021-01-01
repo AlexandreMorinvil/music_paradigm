519a2badd72fa3fd802ecbc4dcb90690b37d8eef
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-01-01 00:22:02 -0500

    Add the ressoucess compoments

---
+++ music_paradigm_web/src/components/admin/ressources/widget-file-browser/file-browser.widget.vue
@@ -0,0 +1,13 @@
+<template>
+	<file-browser :axiosConfig="{ baseURL: 'http://localhost:4000' }" />
+</template>
+
+<script>
+import FileBrowser from 'vuetify-file-browser';
+
+export default {
+	components: {
+		FileBrowser,
+	},
+};
+</script>
