049b17c7dbdfdd4dc0ba85a8cc4fcb6d53afa922
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-01-01 20:40:10 -0500

    integrate the vuetify file browser component communicating with the back-end within the application

--- music_paradigm_api/server.js
+++ music_paradigm_api/server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jwt/jwt.authentication');
 const errorHandler = require('_helpers/error-handler');
+const path = require("path");
+const sdk = require("vuetify-file-browser-server/sdk");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,10 +15,13 @@ app.use(cors());
 
 // serve static
 app.use('/static', express.static('static'));
-app.use('/experiment-ressources', express.static(__dirname + '/../experiment_ressources'));
+app.use('/experiment-ressources', express.static(path.resolve(__dirname, "../experiment_ressources")));
 
 // use JWT auth to secure the api
-app.use(jwt());
+// app.use(jwt());
+
+// get AWS configuration from process.env
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_S3_BUCKET, FILEBROWSER_AWS_ROOT_PATH } = process.env;
 
 // api routes
 app.use('/admin-sessions', require('admin-sessions/admin-sessions.controller'));
@@ -24,6 +29,14 @@ app.use('/account', require('account/account.controller'));
 app.use('/experiments', require('experiments/experiments.controller'));
 app.use('/curriculums', require('curriculums/curriculums.controller'));
 app.use('/users', require('users/users.controller'));
+
+// setup routes for file browser
+app.use("/storage", sdk.Router([
+    new sdk.LocalStorage(path.resolve(__dirname, "../experiment_ressources")),
+    new sdk.S3Storage(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_S3_BUCKET, FILEBROWSER_AWS_ROOT_PATH)
+], {
+    uploadPath: path.resolve(__dirname, "../experiment_ressources")
+}));
 
 // global error handler
 app.use(errorHandler);
--- music_paradigm_web/src/components/admin/ressources/widget-file-browser/file-browser.widget.vue
+++ music_paradigm_web/src/components/admin/ressources/widget-file-browser/file-browser.widget.vue
@@ -1,13 +1,35 @@
 <template>
-	<file-browser :axiosConfig="{ baseURL: 'http://localhost:4000' }" />
+	<div data-app class="file-browser-container">
+		<file-browser :axiosConfig="axiosConfig" />
+	</div>
 </template>
 
 <script>
+import { authHeader, url } from '@/_helpers';
 import FileBrowser from 'vuetify-file-browser';
 
 export default {
 	components: {
 		FileBrowser,
 	},
+	data() {
+		return {};
+	},
+	computed: {
+		axiosConfig() {
+			return {
+				baseURL: url.baseUrl(),
+				timeout: 10000,
+				auth: authHeader(),
+			};
+		},
+	},
 };
 </script>
+
+
+<style scoped>
+.file-browser-container {
+	font-size: 2em;
+}
+</style>
