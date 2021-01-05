90ddb675289be887c8a657701f25701b33a28ea6
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-01-04 23:24:24 -0500

    Attach the pre experiment action to the 'Run Today's Experiment' Button

---
+++ music_paradigm_web/src/components/user/home/due-experiment-button.component.vue
@@ -0,0 +1,24 @@
+<template>
+	<button v-on:click="startDueSession" class="main-button">Run Today’s Session</button>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		...mapActions('session', ['fetchDueExperimentSession']),
+		startDueSession() {
+			this.fetchDueExperimentSession();
+		},
+	},
+	computed: {
+		...mapGetters('account', ['']),
+	},
+};
+</script>
+
+<style scoped></style>
