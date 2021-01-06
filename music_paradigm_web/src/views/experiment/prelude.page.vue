4a7f7faff4e8377e3f53a0e93762559b42ded9f7
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-01-05 23:37:01 -0500

    Fix experiment prelude unable to tell the experiment to start when the piano is initialized outside of the experiment

--- music_paradigm_web/src/views/experiment/prelude.page.vue
+++ music_paradigm_web/src/views/experiment/prelude.page.vue
@@ -12,18 +12,42 @@ import { ExperimentEventBus, experimentEvents } from '@/_services/experiment-eve
 import { mapGetters } from 'vuex';
 
 export default {
+	data() {
+		return {
+			isIntervalStarted: false,
+			repeaterUniqieID: 0,
+			REPEATITION_INTERVAL: 500,
+		};
+	},
 	computed: {
 		...mapGetters('piano', ['isPianoInitialized']),
 		textToDisplay() {
 			if (!this.isPianoInitialized) return 'Loading...';
 			else return 'Ready';
 		},
+		isReadyToStart() {
+			return this.isPianoInitialized;
+		},
+	},
+	methods: {
+		sendStartSignal() {
+			// This strategy of repeating the signal periodically is used in case the signal is
+			// sent too early and is not caught but the parent component at the first emition
+			if (this.isIntervalStarted) return;
+			this.repeaterUniqieID = setInterval(function () {
+				ExperimentEventBus.$emit(experimentEvents.EVENT_EXPERIMENT_READY);
+			}, this.REPEATITION_INTERVAL);
+			this.isIntervalStarted = true;
+		},
+	},
+	beforeDestroy() {
+		clearInterval(this.repeaterUniqieID);
 	},
 	watch: {
-		isPianoInitialized: {
+		isReadyToStart: {
 			immediate: true,
 			handler: function (isReady) {
-				if (isReady) ExperimentEventBus.$emit(experimentEvents.EVENT_EXPERIMENT_READY);
+				if (isReady) this.sendStartSignal();
 			},
 		},
 	},
