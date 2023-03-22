425e01ea2cd40a6ce235a035d09c5ebaebe79c11
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2023-03-22 18:25:52 -0400

    In the users management board, Display the 'create user with curriculum' button when we edit the username (and have a curriculum selected)

--- music_paradigm_web/src/components/admin/users/widget-manage-user-progression/section-user-curriculum/user-curriculum.section.vue
+++ music_paradigm_web/src/components/admin/users/widget-manage-user-progression/section-user-curriculum/user-curriculum.section.vue
@@ -35,6 +35,7 @@ export default {
         WidgetSectionContainer,
     },
     computed: {
+        ...mapGetters('managementUsers', ['hasEditedUserUsername']),
         ...mapGetters('managementUsers/selection', ['hasSelectedUser']),
         ...mapGetters('managementUsers/progressions', [
             'hasEditedUserProgressionAssignedParameters',
@@ -54,7 +55,8 @@ export default {
                 this.hasEditedUserProgressionAssignedParameters;
         },
         hasCreateUserWithCurriculumButton() {
-            return !this.hasSelectedUser && this.hasUserProgressionEditionCurriculumReference;
+            return this.hasUserProgressionEditionCurriculumReference &&
+                (!this.hasSelectedUser || this.hasEditedUserUsername);
         }
     }
 };
