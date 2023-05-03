a36f7fe660694ac9d80967867c65a0cd7465e313
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2023-05-03 16:21:46 -0400

    Remove the old 'log management widgets' from the tasks management page

--- music_paradigm_web/src/views/admin/experiments.page.vue
+++ music_paradigm_web/src/views/admin/experiments.page.vue
@@ -8,14 +8,6 @@
 			<experiments-launcher-widget />
 		</widget-content-frame>
 
-		<widget-content-frame title="Administrators Task Data">
-			<experiments-admin-logs-widget />
-		</widget-content-frame>
-
-		<widget-content-frame title="Users Task Data">
-			<experiments-users-logs-widget />
-		</widget-content-frame>
-
 		<TasksDataWidget />
 		<widget-content-frame title="Tasks List">
 			<tasks-list-widget />
@@ -27,12 +19,10 @@
 import AdminPageContentFrame from '@/components/content-frame/admin-page-content-frame.component.vue';
 import ExperimentsEditor from '@/components/admin/tasks/ExperimentsEditorWidget.vue';
 
-import ExperimentsAdminLogsWidget from '@/components/admin/tasks/widget-admin-logs/experiments-admin-logs.widget.vue';
 import ExperimentsLauncherWidget from '@/components/admin/tasks/widget-launcher/experiments-launcher.widget.vue';
 import TasksDataWidget from '@/components/admin/tasks/widget-task-data/task-data.widget.vue';
 import TasksListWidget from '@/components/admin/tasks/widget-list/tasks-list.widget.vue';
 
-import ExperimentsUsersLogsWidget from '@/components/admin/tasks/widget-users-logs/experiments-users-logs.widget.vue';
 import WidgetContentFrame from '@/components/content-frame/widget-content-frame.component.vue';
 
 export default {
@@ -41,8 +31,6 @@ export default {
 		WidgetContentFrame,
 		experimentsEditor: ExperimentsEditor,
 		ExperimentsLauncherWidget,
-		ExperimentsAdminLogsWidget,
-		ExperimentsUsersLogsWidget,
 		TasksDataWidget,
 		TasksListWidget,
 	},
