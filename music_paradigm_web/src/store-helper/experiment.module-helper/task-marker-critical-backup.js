a7905c4a5e3fd0d9c321f59aa211166e5618b254
Work Forward <alexandre.morinvil@outlook.com>
2024-10-01 01:57:34 +0800

    Minor adjustments in the code to prevent errors and remove an umnecessary console.log(...)

--- music_paradigm_web/src/store-helper/experiment.module-helper/task-marker-critical-backup.js
+++ music_paradigm_web/src/store-helper/experiment.module-helper/task-marker-critical-backup.js
@@ -9,7 +9,7 @@ function performCriticalAdjustment(state) {
 
     // Retrieve needed variables
     const { cursor, criticalBackup } = state;
-    const { cursor: criticalCursor, state: criticalState } = criticalBackup;
+    const { cursor: criticalCursor, state: criticalState } = criticalBackup ?? {};
 
     // Verify if the critical checkpoints must be used
     const useCriticalCheckpoints = state.settings.useCriticalCheckpoints;
@@ -23,7 +23,7 @@ function performCriticalAdjustment(state) {
     // Verify is the index of the current cursor is behind the index.
     // If it is, we force the session to return to the critical position in the task.
     const currentIndex = cursor.current.index;
-    const criticalIndex = criticalCursor?.current.index;
+    const criticalIndex = criticalCursor?.current?.index;
     if (criticalIndex && currentIndex < criticalIndex) {
         state.state = JSON.parse(JSON.stringify(criticalState));
         state.cursor = JSON.parse(JSON.stringify(criticalCursor));
@@ -34,12 +34,12 @@ function updateCriticalBackup(state) {
 
     // Retrieve needed variables
     const { cursor, state: currentState, criticalBackup } = state;
-    const { cursor: criticalCursor } = criticalBackup;
+    const { cursor: criticalCursor } = criticalBackup  ?? {};
 
     // If the index of the cursor is ahead or eaqual to the critical cursor, we update the critical
     // backup
     const currentIndex = cursor.current.index;
-    const criticalIndex = criticalCursor?.current.index;
+    const criticalIndex = criticalCursor?.current?.index;
     if (!criticalIndex || currentIndex >= criticalIndex) {
         state.criticalBackup = { state: currentState, cursor: cursor };
     }
